89794f84ec91573c7e4791a83214e7f8c29a47e5
Tracey <tracey.wilhelmsen@gmail.com>
2017-11-11 01:36:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const currentUser = "Grace Hopper";
 
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
-const excitedWelcomeMessage = welcomeMessage.toUpperCase;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
