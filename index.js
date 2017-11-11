afe60ec09fbc30222c7f433be8b09201b7214c2e
Tracey <tracey.wilhelmsen@gmail.com>
2017-11-11 02:14:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
-const shortGreeting = `Welcome, ${currentUser}`;
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}`;
+
+
